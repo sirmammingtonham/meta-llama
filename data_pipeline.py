cd0b9227336807737fbb706ed4e3c72602c605dc
sunil-2000 <sunilsabnis10@gmail.com>
2023-04-28 11:34:16 -0400

    inital training data seeds

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,29 +1,56 @@
 import csv
+import os
+import openai
 import numpy as np
+from tqdm import tqdm
 from datasets import load_dataset
 
 np.random.seed(0)
 
-class Pipeline:
-    def __init__(self, tasks_dir: str, task_seed_size: int, sk: str) -> None:
+EXCLUDED_TASKS = ["cifar10_classification", "mnist_ascii"]
+
+
+class GenerationPipeline:
+    def __init__(
+        self, tasks_dir: str, train_dir: str, task_seed_size: int, sk: str
+    ) -> None:
         """
         tasks_dir: name of file (csv) of tasks to augment
+        train_dir: file path to directory to write data to
+        k: amount of in-context examples for generation
         task_seed_size: size of seed examples per task
         sk: OpenAI api key
         """
+        openai.api_key = sk
+
         with open(tasks_dir, newline="") as f:
             reader = csv.reader(f)
             tasks = [row[-1] for row in reader]
         tasks = tasks[1:]
         f.close()
+        tasks = list(filter(lambda task: task not in EXCLUDED_TASKS, tasks))
 
-        img_tasks = ["cifar10_classification", "mnist_ascii"]
+        # train test split wrt tasks
+        idx = np.random.permutation(len(tasks))
+        split = int(len(tasks) * 0.8)  # 80, 20 split
+        train_idx, test_idx = idx[:split], idx[split:]
+        self.train_tasks = [tasks[i] for i in train_idx]
+        self.test_tasks = [tasks[i] for i in test_idx]
 
-        self.seed_tasks = {}
-        self.missing_tasks = []
-        for task in tasks:
-            print(f"processing: {task}")
-            if task in img_tasks:
+        # initialize seed task jsons in training directory
+        self.missing_tasks = self._init_task_seeds(task_seed_size, train_dir)
+
+    def _init_task_seeds(self, task_seed_size: int, train_dir: str):
+        """
+        only augment training tasks
+        train_dir: file path to directory to write data to
+        task_seed_size: size of seed examples per task
+        return missing tasks lists: [str]
+        """
+        missing_tasks = []
+        for task in tqdm(self.train_tasks):
+            path = f"{train_dir}/{task}.json"
+            if os.path.exists(os.path.join(os.getcwd(), path)):  # already loaded
                 continue
             try:
                 D_task = load_dataset("tasksource/bigbench", task, split="train")
@@ -31,28 +58,51 @@ class Pipeline:
                     low=0, high=len(D_task), size=task_seed_size
                 ).tolist()
                 D_init = D_task.select(k_idx)
-                self.seed_tasks[task] = D_init
+                D_init.to_json(path)  # write to json
             except:
-                print(f"task not found: {task}")
-                self.missing_tasks.append(task)
+                missing_tasks.append(task)
+        return missing_tasks
 
-        print(self.seed_tasks.keys())
-        print(self.missing_tasks)
-
-    def augment(self):
+    def augment(self, k: int, train_dir: str):
         """
-        for all tasks:
+        k: amount of in-context examples for generation
+        train_dir: file path to directory containing train data json files
+
+        for all train tasks:
         (0) Sample k examples from task
           * format k examples into prompt
         (1) GPT to generate additional examples
           * process output into json (inputs, m)
+          * deterministic generation mode (temp=0, top_p=1)
         (2) Filter additional examples
-        (3) add augmented examples into task set
+        (3) add augmented examples into tasks super set
         """
+        train_tasks = os.listdir(train_dir)
+        for task in train_tasks:
+            D_task = load_dataset("json", data_files=f"{train_dir}/{task}")
+            k_idx = np.random.randint(low=0, high=len(D_task), size=k).tolist()
+            D_subset = D_task.select(k_idx)
+        pass
+
+    def to_prompt(self, data):
+        """
+        data: row from dataset
+        """
+        pass
+
+    def make_request(self):
         pass
 
     def _filter(self):
         pass
 
+    def _field_check(self):
+        pass
 
-# Pipeline("./bigbench_metadata/mc_tasks.csv", 8, "hello")
+    def _rougel_check(self):
+        pass
+
+
+# k = 4
+# g = GenerationPipeline("./bigbench_metadata/mc_tasks.csv", "training_data", 8, "hello")
+# g.augment(k, "training_data")
