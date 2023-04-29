fbaa2f44b1a03b4740f6ecfbe9db532d4a43cd74
sunil-2000 <sunilsabnis10@gmail.com>
2023-04-29 16:06:35 -0400

    chat request, prompt format experiments (notebook), question extraction from response

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,9 +1,12 @@
 import csv
+import json
+import ast
 import os
 import openai
 import numpy as np
 from tqdm import tqdm
-from datasets import load_dataset
+from typing import List, Dict
+from datasets import load_dataset, Dataset
 
 np.random.seed(0)
 
@@ -43,6 +46,7 @@ class GenerationPipeline:
     def _init_task_seeds(self, task_seed_size: int, train_dir: str):
         """
         only augment training tasks
+
         train_dir: file path to directory to write data to
         task_seed_size: size of seed examples per task
         return missing tasks lists: [str]
@@ -63,11 +67,9 @@ class GenerationPipeline:
                 missing_tasks.append(task)
         return missing_tasks
 
-    def augment(self, k: int, train_dir: str):
+    def augment(self, k: int, train_dir: str, total_qs: int) -> None:
         """
-        k: amount of in-context examples for generation
-        train_dir: file path to directory containing train data json files
-
+        TODO (unfinished)
         for all train tasks:
         (0) Sample k examples from task
           * format k examples into prompt
@@ -76,22 +78,101 @@ class GenerationPipeline:
           * deterministic generation mode (temp=0, top_p=1)
         (2) Filter additional examples
         (3) add augmented examples into tasks super set
+
+        k: amount of in-context examples for generation
+        train_dir: file path to directory containing train data json files
+        total_qs: total number of question to generate (including in-context
+        examples)
         """
         train_tasks = os.listdir(train_dir)
         for task in train_tasks:
             D_task = load_dataset("json", data_files=f"{train_dir}/{task}")
             k_idx = np.random.randint(low=0, high=len(D_task), size=k).tolist()
             D_subset = D_task.select(k_idx)
+            prompt = self.to_prompt(D_subset, total_qs)
+            # make request
+            msg_content = self.make_request(prompt)
+            # process request
+            self.process_request(msg_content, task)
         pass
 
-    def to_prompt(self, data):
+    def to_prompt(self, dataset: Dataset, total_qs: int) -> str:
         """
-        data: row from dataset
+        TODO (review, add task description to prompt)
+        dataset: subset to embed as context for prompt
+        total_qs: total questions to generate including embedded questions
         """
-        pass
+        keys = ["inputs", "targets", "multiple_choice_targets"]
+        # each key has same amount of values want to map each value of each key with each other
+        ds_dict = dataset.to_dict()
+        n_examples = len(ds_dict["inputs"])
 
-    def make_request(self):
-        pass
+        examples = []
+        for i in range(n_examples):
+            example = {}
+            for key in keys:
+                example[key] = ds_dict[key][i]
+            examples.append(example)
+
+        prompt = f"Generate a series of {total_qs} questions in a valid JSON format:"
+        # add description of task? 
+        for i, e in enumerate(examples):
+            prompt += f"\nQuestion {i}:\n{e}"
+        prompt += f"\nQuestion {n_examples}:"
+
+        return prompt
+
+    def make_request(self, prompt: str, model: str = "gpt-4") -> str:
+        """
+        Assumes only sending one prompt (e.g., not sending list of prompts)
+
+        prompt: includes in-context examples
+        model: model used for generation
+        """
+        msg_content = ""
+        try:
+            completion = openai.ChatCompletion.create(
+                model=model,
+                messages=[{"role": "system", "content": prompt}],  # change role??
+                temperature=0,
+            )
+            msg_content = completion['choices'][0]['message']['content']
+        except openai.error.OpenAIError as e:
+            print(f"OpenAIError: {e}.")
+
+        return msg_content
+
+    def process_request(self, msg_content:str, task:str) -> None:
+        """
+        TODO (review, add filtering)
+        processes msg_content from GPT
+        (1) converts msg_content to list of dictionaries 
+        (2) applying filters to dictionary examples
+        (3) append list of dictionaries to json (filename: "{task}_generated.json")
+
+        res: message content from GPT
+        task: name of task generated questions correspond to
+        """
+        generated_examples = self._generation_to_dicts(msg_content)
+        # apply filters here
+        with open(f"{task}_generated.json", "a") as f:
+          json.dump(generated_examples, f)
+
+    def _generation_to_dicts(self, msg_content:str) -> List[Dict]:
+      """
+      TODO (review)
+      returns list of dictionary representing generated questions
+      """
+      generate_qs = msg_content.split("Question") # change split word?
+      generated_json_examples = []
+      for i, q in enumerate(generate_qs):
+        if i == 0: # end of prompt = "... Question i:"
+            msg_dict = ast.literal_eval(q)
+        else: # cut off Question:\n that comes before the {<json_question>}
+            q = q[q.find('\n'):]
+            q = q.rstrip("\n")
+            msg_dict = ast.literal_eval(q)
+      generated_json_examples.append(msg_dict)
 
     def _filter(self):
         pass
