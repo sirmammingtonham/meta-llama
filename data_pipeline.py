67178faeb47e3fcf28c7fa641b3e88b8cd94a143
sunil-2000 <sunilsabnis10@gmail.com>
2023-05-01 14:25:40 -0400

    added filtering to generatio pipeline, initial implementation of full generation loop

--- data_pipeline.py
+++ data_pipeline.py
@@ -3,9 +3,13 @@ import json
 import ast
 import os
 import openai
+import re
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
-from typing import List, Dict
+from collections import defaultdict
+from rouge_score import rouge_scorer
+from typing import List, Dict, Tuple
 from datasets import load_dataset, Dataset
 
 np.random.seed(0)
@@ -15,16 +19,30 @@ EXCLUDED_TASKS = ["cifar10_classification", "mnist_ascii"]
 
 class GenerationPipeline:
     def __init__(
-        self, tasks_dir: str, train_dir: str, task_seed_size: int, sk: str
+        self,
+        tasks_dir: str,
+        train_dir: str,
+        train_dir_gen: str,
+        error_dir: str,
+        task_seed_size: int,
+        sk: str,
+        filter_errors: str = "filter_errors.csv",
     ) -> None:
         """
         tasks_dir: name of file (csv) of tasks to augment
         train_dir: file path to directory to write data to
+        train_dir_gen: file path to dump generated instances
+        error_dir: file path to dump error related data
         k: amount of in-context examples for generation
         task_seed_size: size of seed examples per task
+        filter_errors: file name (csv) to write filter errors to
         sk: OpenAI api key
         """
         openai.api_key = sk
+        self.train_dir = train_dir
+        self.train_dir_gen = train_dir_gen
+        self.error_dir = error_dir
+        self.filter_errors = filter_errors
 
         with open(tasks_dir, newline="") as f:
             reader = csv.reader(f)
@@ -40,10 +58,28 @@ class GenerationPipeline:
         self.train_tasks = [tasks[i] for i in train_idx]
         self.test_tasks = [tasks[i] for i in test_idx]
 
-        # initialize seed task jsons in training directory
+        # # initialize seed task jsons in training directory
         self.missing_tasks = self._init_task_seeds(task_seed_size, train_dir)
+        self.train_tasks = filter(
+            lambda x: x not in self.missing_tasks, self.train_tasks
+        )
 
-    def _init_task_seeds(self, task_seed_size: int, train_dir: str):
+        # initialize json files to dump generated examples into
+        if not os.path.exists(self.train_dir_gen):
+            os.makedirs(self.train_dir_gen)
+
+        # initialize error recording directory
+        if not os.path.exists(self.error_dir):
+            os.makedirs(self.error_dir)
+
+        for task in self.train_tasks:
+            with open(f"{self.train_dir_gen}/{task}.json", "w") as f:
+                json.dump([], f)
+            f.close()
+
+        self.descr_df = pd.read_csv("./bigbench_metadata/task_descriptions.csv")
+
+    def _init_task_seeds(self, task_seed_size: int, train_dir: str) -> List[str]:
         """
         only augment training tasks
 
@@ -62,14 +98,17 @@ class GenerationPipeline:
                     low=0, high=len(D_task), size=task_seed_size
                 ).tolist()
                 D_init = D_task.select(k_idx)
-                D_init.to_json(path)  # write to json
+                is_generated = [False] * len(D_init)
+                D_init = D_init.add_column("is_generated", is_generated)
+                D_init = D_init.add_column("true_idx", k_idx)
+                D_init.remove_columns("idx")
+                D_init.to_json(path, orient="records", lines=False)
             except:
                 missing_tasks.append(task)
         return missing_tasks
 
     def augment(self, k: int, train_dir: str, total_qs: int) -> None:
         """
-        TODO (unfinished)
         for all train tasks:
         (0) Sample k examples from task
           * format k examples into prompt
@@ -85,22 +124,39 @@ class GenerationPipeline:
         examples)
         """
         train_tasks = os.listdir(train_dir)
-        for task in train_tasks:
+        for i, task in enumerate(train_tasks):
             D_task = load_dataset("json", data_files=f"{train_dir}/{task}")
             k_idx = np.random.randint(low=0, high=len(D_task), size=k).tolist()
+            # TODO weighted sample based on how many times selected from ds?
             D_subset = D_task.select(k_idx)
-            prompt = self.to_prompt(D_subset, total_qs)
+            prompt = self.to_prompt(D_subset, total_qs, task)
             # make request
             msg_content = self.make_request(prompt)
             # process request
-            self.process_request(msg_content, task)
-        pass
+            filter_summary = self.process_request(msg_content, task)
+            # record error summary
+            with open(f"{self.error_dir}/{self.filter_errors}", "a+") as f:
+                # task, parse filter, rl filter, fc filter <-- columns
+                filter_summary["task"] = task
+                dict_writer = csv.DictWriter(
+                    f,
+                    fieldnames=[
+                        "task",
+                        "parse_filters",
+                        "field_check_filters",
+                        "rougel_filters",
+                    ],
+                )
+                if i == 0:
+                    dict_writer.writeheader()
+                dict_writer.writerow(filter_summary)
+            f.close()
 
-    def to_prompt(self, dataset: Dataset, total_qs: int) -> str:
+    def to_prompt(self, dataset: Dataset, total_qs: int, task: str) -> str:
         """
-        TODO (review, add task description to prompt)
         dataset: subset to embed as context for prompt
         total_qs: total questions to generate including embedded questions
+        task: task name
         """
         keys = ["inputs", "targets", "multiple_choice_targets"]
         # each key has same amount of values want to map each value of each key with each other
@@ -115,7 +171,9 @@ class GenerationPipeline:
             examples.append(example)
 
         prompt = f"Generate a series of {total_qs} questions in a valid JSON format:"
-        # add description of task? 
+        task_descr = self.descr_df[(self.desc_df == task)].values[0]
+        prompt += f"\nTASK DESCRIPTION: {task_descr}"
+
         for i, e in enumerate(examples):
             prompt += f"\nQuestion {i}:\n{e}"
         prompt += f"\nQuestion {n_examples}:"
@@ -136,54 +194,113 @@ class GenerationPipeline:
                 messages=[{"role": "system", "content": prompt}],  # change role??
                 temperature=0,
             )
-            msg_content = completion['choices'][0]['message']['content']
+            msg_content = completion["choices"][0]["message"]["content"]
         except openai.error.OpenAIError as e:
             print(f"OpenAIError: {e}.")
 
         return msg_content
 
-    def process_request(self, msg_content:str, task:str) -> None:
+    def process_request(self, msg_content: str, task: str) -> Dict:
         """
-        TODO (review, add filtering)
         processes msg_content from GPT
-        (1) converts msg_content to list of dictionaries 
+        (1) parse msg_content to python dict
         (2) applying filters to dictionary examples
-        (3) append list of dictionaries to json (filename: "{task}_generated.json")
+        (4) write filtered messages to appropriate task db (json)
 
         res: message content from GPT
         task: name of task generated questions correspond to
         """
-        generated_examples = self._generation_to_dicts(msg_content)
-        # apply filters here
-        with open(f"{task}_generated.json", "a") as f:
-          json.dump(generated_examples, f)
+        questions_parsed, parsed_errs = self._generation_to_dicts(msg_content)
+        questions_fc_pass, fc_errs = self._field_check(questions_parsed)
+        added_questions, rl_errs = self._rougel_check(questions_fc_pass)
 
-    def _generation_to_dicts(self, msg_content:str) -> List[Dict]:
-      """
-      TODO (review)
-      returns list of dictionary representing generated questions
-      """
-      generate_qs = msg_content.split("Question") # change split word?
-      generated_json_examples = []
-      for i, q in enumerate(generate_qs):
-        if i == 0: # end of prompt = "... Question i:"
-            msg_dict = ast.literal_eval(q)
-        else: # cut off Question:\n that comes before the {<json_question>}
-            q = q[q.find('\n'):]
-            q = q.rstrip("\n")
-            msg_dict = ast.literal_eval(q)
-      generated_json_examples.append(msg_dict)
+        filter_summary = {
+            "parse_filters": parsed_errs,
+            "field_check_filters": fc_errs,
+            "rougel_filters": rl_errs,
+        }
 
-    def _filter(self):
-        pass
+        with open(f"{self.train_dir_gen}/{task}.json", "r") as f:
+            task_ds = json.load(f)
+        f.close()
 
-    def _field_check(self):
-        pass
+        task_ds.extend(added_questions)
 
-    def _rougel_check(self):
-        pass
+        with open(f"{self.train_dir_gen}/{task}.json", "w") as f:
+            json.dump(task_ds, f)
+        f.close()
 
+        return filter_summary
 
-# k = 4
-# g = GenerationPipeline("./bigbench_metadata/mc_tasks.csv", "training_data", 8, "hello")
-# g.augment(k, "training_data")
+    def _generation_to_dicts(self, msg_content: str) -> Tuple[List[Dict], int]:
+        """
+        returns list of dictionary representing generated questions
+        """
+        # change split word? what if 'Question: \d' in examples
+        generate_qs = re.split("Question \d:", msg_content)
+        generated_dict_examples, errs = [], 0
+        for i, q in enumerate(generate_qs):
+            if i == 0:  # end of prompt = "... Question i:"
+                try:
+                    msg_dict = ast.literal_eval(q)
+                except:
+                    print(f"unable to convert to dict: {q}")
+                    errs += 1
+            else:  # cut off Question:\n that comes before the {<json_question>}
+                q = q[q.find("\n") :]
+                q = q.rstrip("\n")
+                try:
+                    msg_dict = ast.literal_eval(q)
+                except:
+                    print(f"unable to convert to dict: {q}")
+                    errs += 1
+            generated_dict_examples.append(msg_dict)
+
+        return generated_dict_examples, errs
+
+    def _field_check(self, questions: List[Dict]) -> Tuple[List[Dict], int]:
+        """
+        questions: generated question list in dict format
+        """
+        keys = set(["inputs", "targets", "multiple_choice_targets"])
+        out, errs = [], 0
+        for q in questions:
+            if len(keys.difference(set(q.keys()))) == 0:
+                out.append(q)
+            else:
+                errs += 1
+        return out, errs
+
+    def _rougel_check(self, task: str, questions: List[Dict]) -> Tuple[List[Dict], int]:
+        """
+        assumes questions pass _field_check
+        task: task name
+        questions: generated question list in dict format (e.g.,
+        {inputs:<str>, targets:<str>, multiple_choice_targets:<str>})
+        """
+        # get current examples in db
+        with open(f"{self.train}/{task}.json", "r") as f:
+            ds_init = json.load(f)
+        f.close()
+        with open(f"{self.train_dir_gen}/{task}.json", "r") as f:
+            existing_generated_questions = json.load(f)
+        f.close()
+
+        ds_init.extend(existing_generated_questions)
+
+        ds_inputs = [question["inputs"] for question in ds_init]
+        generated_inputs = [question["inputs"] for question in questions]
+
+        scorer = rouge_scorer.RougeScorer(
+            ["rougeL"], use_stemmer=False
+        )  # same as self-instruct setting
+        scores, excluded_qs, errs = defaultdict(list), set(), 0
+        for i, existing_q in enumerate(ds_inputs):
+            for j, new_q in enumerate(generated_inputs):
+                score = scorer.score(new_q, existing_q)["rougeL"].fmeasure
+                scores[j].append((i, score))
+                if score > 0.7:
+                    excluded_qs.add(j)
+                    errs += 1
+
+        return [q for i, q in enumerate(questions) if i not in excluded_qs], errs
